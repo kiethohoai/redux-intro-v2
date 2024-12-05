fcbd93aa8720e54948511fb46f9412dc403bd866
David Ho <kietho.060193>
2024-12-05 17:30:15 +0700

    006 Adding More State Customer

--- src/store.js
+++ src/store.js
@@ -1,26 +1,33 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from 'redux';
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
-  loanPurpose: "",
+  loanPurpose: '',
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: '',
+  nationalId: '',
+  createdAt: '',
+};
+
+// todo accountReducer
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
-    case "account/deposit":
+    case 'account/deposit':
       return {
         ...state,
         balance: state.balance + action.payload,
       };
 
-    case "account/withdraw":
+    case 'account/withdraw':
       return {
         ...state,
         balance: state.balance - action.payload,
       };
 
-    case "account/requestLoan":
+    case 'account/requestLoan':
       if (state.loan > 0) return state;
       return {
         ...state,
@@ -28,17 +35,17 @@ function reducer(state = initialState, action) {
         loanPurpose: action.payload.loanPurpose,
       };
 
-    case "account/payLoan":
+    case 'account/payLoan':
       return {
         ...state,
         loan: 0,
-        loanPurpose: "",
+        loanPurpose: '',
         balance: state.balance - state.loan,
       };
 
-    case "account/reset":
+    case 'account/reset':
       return {
-        ...initialState,
+        ...initialStateAccount,
       };
 
     default:
@@ -46,28 +53,73 @@ function reducer(state = initialState, action) {
   }
 }
 
+// todo customerReducer
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalId: action.payload.nationalId,
+        createdAt: action.payload.createdAt,
+      };
+
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+//todo Account Actions
 function deposit(value) {
-  return { type: "account/deposit", payload: value };
+  return { type: 'account/deposit', payload: value };
 }
 function withdraw(value) {
-  return { type: "account/withdraw", payload: value };
+  return { type: 'account/withdraw', payload: value };
 }
 function requestLoan(loan, loanPurpose) {
-  return { type: "account/requestLoan", payload: { loan, loanPurpose } };
+  return { type: 'account/requestLoan', payload: { loan, loanPurpose } };
 }
 function payLoan() {
-  return { type: "account/payLoan" };
+  return { type: 'account/payLoan' };
 }
 
 function reset() {
-  return { type: "account/reset" };
+  return { type: 'account/reset' };
 }
 
-const store = createStore(reducer);
-store.dispatch(deposit(500));
-store.dispatch(withdraw(300));
-store.dispatch(requestLoan(1000, "By Car"));
-store.dispatch(payLoan());
-store.dispatch(reset());
+//todo Customer Action
+function createCustomer(fullName, nationalId) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalId, createdAt: new Date() },
+  };
+}
+function updateName(fullName) {
+  return { type: 'customer/updateName', payload: fullName };
+}
 
+// todo Store
+const rootRecuder = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootRecuder);
+
+/* Account */
+store.dispatch(deposit(1200));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, 'By Car'));
+store.dispatch(payLoan());
+console.log(store.getState());
+
+/* Customer */
+store.dispatch(createCustomer('John Doe', '123456789'));
+console.log(store.getState());
+store.dispatch(updateName('John Ho'));
 console.log(store.getState());
